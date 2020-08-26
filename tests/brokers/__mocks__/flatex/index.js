a7494b898d798fc370721589c9929c3ea506ccca
David Dreher <david@md-web-solutions.de>
2020-08-26 09:27:04 +0200

    [flatex] Adjust logic to parse multiple pages and pages with multiple orders (#82)
    * Add test case
    
    * eslint stuff
    
    * Add support for multiple activities in one page. Fix #81
    
    Co-authored-by: David Dreher <dredav@users.noreply.github.com>

--- tests/brokers/__mocks__/flatex/index.js
+++ tests/brokers/__mocks__/flatex/index.js
@@ -1,18 +1,17 @@
 export const buySamples = [
-    require('./buy1.json'),
-    require('./buy2_etf.json'),
-    require('./buy3.json'),
-    require('./buy4.json'),
-    require('./buy5.json'),
+  require('./buy1.json'),
+  require('./buy2_etf.json'),
+  require('./buy3.json'),
+  require('./buy4.json'),
+  require('./buy5.json'),
 ];
 
-export const sellSamples = [
-    require('./sell1.json'),
-    require('./sell2.json'),
-]
+export const sellSamples = [require('./sell1.json'), require('./sell2.json')];
 
-export const dividendsSamples  = [
-    require('./dividend1.json'),
-    require('./dividend2.json'),
-    require('./dividend3.json'),
+export const dividendsSamples = [
+  require('./dividend1.json'),
+  require('./dividend2.json'),
+  require('./dividend3.json'),
 ];
+
+export const mixedPageSamples = [require('./mixed-pages/0.json')];
