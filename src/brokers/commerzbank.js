0211a886d33dc0afcbd5fdbfe94a5914bf1597d4
David Christian Holin <61882185+SirGibihm@users.noreply.github.com>
2020-11-21 14:41:25 +0100

    Added support for transaction report documents. Also implemented the ability to use TransferIn and TransferOUt which are not yet supported by the T1 backend. They are commented out for the moment.

--- src/brokers/commerzbank.js
+++ src/brokers/commerzbank.js
@@ -4,6 +4,7 @@ import Big from 'big.js';
 
 import { parseGermanNum, validateActivity } from '@/helper';
 
+// These functions are for single transaction documents
 const findAmountBuy = textArr => {
   const amountIndex = textArr.findIndex(t => t.includes('IBAN')) - 1;
   return parseGermanNum(textArr[amountIndex]);
@@ -55,24 +56,15 @@ const findDateForeignDividend = textArr => {
   return format(parse(date_string, 'dd.MM.yyyy', new Date()), 'yyyy-MM-dd');
 };
 
-const findWknBuy = textArr => {
-  return textArr[textArr.findIndex(t => t.includes('Registered')) - 1];
-};
+const findWknBuy = textArr => textArr[textArr.findIndex(t => t.includes('Registered')) - 1];
 
-const findWknDividend = textArr => {
-  return textArr[textArr.findIndex(t => t.includes('WKN')) + 3];
-};
+const findWknDividend = textArr => textArr[textArr.findIndex(t => t.includes('WKN')) + 3];
 
-const findWknForeignDividend = textArr => {
-  return textArr[textArr.findIndex(t => t.includes('WKN/ISIN')) + 3];
-};
+const findWknForeignDividend = textArr => textArr[textArr.findIndex(t => t.includes('WKN/ISIN')) + 3];
 
-const findIsinDividend = textArr => {
-  return textArr[textArr.findIndex(t => t.includes('ISIN')) + 3];
-};
-const findIsinForeignDividend = textArr => {
-  return textArr[textArr.findIndex(t => t.includes('STK')) + 2];
-};
+const findIsinDividend = textArr => textArr[textArr.findIndex(t => t.includes('ISIN')) + 3];
+
+const findIsinForeignDividend = textArr => textArr[textArr.findIndex(t => t.includes('STK')) + 2];
 
 const findTaxDividend = textArr => {
   // Foreign Payouts need to be treated differently
@@ -107,25 +99,7 @@ const isDividend = textArr =>
 const isForeignDividend = textArr =>
   textArr.some(t => t.includes('Ertragsgutschrift'));
 
-export const canParsePage = (content, extension) => {
-  //The first PDF Page does not always contain "Commerzbank", thus this ugly
-  //workaround. e. G. dividend_IE00B3RBWM25_1.json
-  if (!Array.isArray(content)) {
-    return undefined;
-  }
-  const joinedContent = content.join('');
-  return (
-    extension === 'pdf' &&
-    (joinedContent.includes('COMMERZBANK') ||
-      joinedContent.includes('Commerzbank') ||
-      joinedContent.includes(
-        'SteuerlicheBehandlung:AusländischeInvestment-Ausschüttung'
-      )) &&
-    (isBuy(content) || isForeignDividend(content) || isDividend(content))
-  );
-};
-
-const parseData = textArr => {
+const parseSingleTransaction = textArr => {
   let activity;
   if (isBuy(textArr)) {
     const foundAmount = Big(findAmountBuy(textArr));
@@ -178,9 +152,174 @@ const parseData = textArr => {
   return validateActivity(activity);
 };
 
-export const parsePages = contents => {
-  const activities = [parseData(contents[0])];
+// These functions are for transaction reports
+const findPriorIndex = (arr, idx, keyArr = ['STK', '/ Sperre']) => {
+  let bckwrdIdx = 1;
+  while (idx-bckwrdIdx >= 0) {
+    if ( keyArr.includes(arr[idx-bckwrdIdx])) {
+      return idx-bckwrdIdx;
+    }
+    bckwrdIdx+=1;
+  }
+  return -1
+}
 
+const isTransactionReport = textArr =>
+  textArr.join('').includes('StückzinsenKurs/ AusschüttungTransaktionsartDatumWKN / ISINEndbetrag');
+
+const parseBuySellTransaction = ( pdfPage, pageIdx, type) => {
+  const txStart = findPriorIndex(pdfPage, pageIdx)+1;
+  const txEndIdx = pdfPage.indexOf('STK', pageIdx);
+  const priceSplitOffset = pdfPage[pageIdx-1].includes(' ') ? 0 : 1;
+  let activity = {
+    broker: 'commerzbank',
+    type: type,
+    company: pdfPage.slice(txStart, pageIdx-2-priceSplitOffset).join(' '),
+    wkn: pdfPage[pageIdx+2],
+    isin: pdfPage[pageIdx+3].split(' ')[1],
+    amount: Math.abs(parseGermanNum(pdfPage[pageIdx-2-priceSplitOffset])),
+    price: Math.abs(parseGermanNum(pdfPage[pageIdx-1-priceSplitOffset])),
+    fee: 0,
+    tax: 0,
+  }
+  // It is not exactly clear what is a fee and what is a tax from the .pdf
+  // Since Buy only has fees it will be a fee for the buy operation and a
+  // tax for the sell operations. This is NOT ENTIRELY CORRECT
+  const payedAmount = Math.abs(parseGermanNum(pdfPage[pageIdx+4]));
+  if ( type === 'Buy' ) {
+    activity.fee = +Big(activity.amount).minus(payedAmount).abs();
+  }
+  else if (type === 'Sell') {
+    activity.tax = +Big(activity.amount).minus(payedAmount).abs();
+  }
+
+  if (txEndIdx-pageIdx > 7) {
+    activity.foreignCurrency = priceSplitOffset === 0 ?
+      pdfPage[pageIdx-1].split(' ')[1] :
+      pdfPage[pageIdx-1];
+    activity.fxRate = parseGermanNum(pdfPage[pageIdx+5]);
+    activity.price = +Big(activity.price).div(activity.fxRate);
+  }
+  const fxOffset = activity.fxRate === undefined ? 0 : 1;
+  activity.shares = Math.abs(parseGermanNum(pdfPage[pageIdx+5+fxOffset]));
+  activity.date = format(parse(pdfPage[pageIdx+6+fxOffset], 'dd.MM.yyyy', new Date()), 'yyyy-MM-dd');
+  if ( type === 'Sell') {
+    return activity;
+  }
+  return activity;
+}
+
+/* const parseTxInOutTransaction = (pdfPage, pageIdx, type) => {
+
+  const txStart = findPriorIndex(pdfPage, pageIdx)+1;
+  const fxOffset = pdfPage[pageIdx+7] === 'STK' ? 1 : 0;
+  return {
+    broker: 'commerzbank',
+    type: type,
+    company: pdfPage.slice(txStart, pageIdx).join(' '),
+    wkn: pdfPage[pageIdx+2],
+    isin: pdfPage[pageIdx+3].split(' ')[1],
+    shares: Math.abs(parseGermanNum(pdfPage[pageIdx+4+fxOffset])),
+    date: format(parse(pdfPage[pageIdx+5+fxOffset], 'dd.MM.yyyy', new Date()), 'yyyy-MM-dd'),
+    tax: 0,
+    fee: 0,
+  }
+} */
+
+const parseDividendTransaction = ( pdfPage, pageIdx) => {
+  const txEndIdx = pdfPage.indexOf('STK', pageIdx);
+  const txStart = findPriorIndex(pdfPage, pageIdx)+1;
+  const priceSplitOffset = pdfPage[pageIdx-1].includes(' ') ? 0 : 1;
+
+  let activity = {
+    broker: 'commerzbank',
+    type: 'Dividend',
+    company: pdfPage.slice(txStart, pageIdx-1-priceSplitOffset).join(' '),
+    wkn: pdfPage[pageIdx+2],
+    isin: pdfPage[pageIdx+3].split(' ')[1],
+    price: Math.abs(parseGermanNum(pdfPage[pageIdx-1-priceSplitOffset])),
+    fee: 0,
+  }
+  if (txEndIdx-pageIdx > 7) {
+    activity.foreignCurrency = priceSplitOffset === 0 ?
+      pdfPage[pageIdx-1].split(' ')[1] :
+      pdfPage[pageIdx-1];
+    activity.fxRate = parseGermanNum(pdfPage[pageIdx+5]);
+    activity.price = +Big(activity.price).div(activity.fxRate);
+  }
+  const fxOffset = activity.fxRate === undefined ? 0 : 1;
+  activity.date = format(parse(pdfPage[pageIdx+6+fxOffset], 'dd.MM.yyyy', new Date()), 'yyyy-MM-dd');
+  activity.shares = Math.abs(parseGermanNum(pdfPage[pageIdx+5+fxOffset]));
+  activity.amount = +Big(activity.shares).times(activity.price);
+  const postTaxAmount = Math.abs(parseGermanNum(pdfPage[pageIdx+4]));
+  // It is unknown from the .pdf file why the net amount and payed out amount
+  // diverge, most likely reason is tax:
+  activity.tax = +Big(activity.amount).minus(postTaxAmount);
+  return activity;
+}
+
+const parseTransactionReport = pdfPages => {
+  let actions = [];
+  for (const pdfPage of pdfPages) {
+    let pageIdx = 0;
+    while (pageIdx <= pdfPage.length) {
+      let activity = undefined;
+      if (pdfPage[pageIdx] === 'Kauf') {
+        activity = parseBuySellTransaction(pdfPage, pageIdx, 'Buy');
+        pageIdx += 6;// A transaction string is followed by 6 least related entries
+      } else if (pdfPage[pageIdx] === 'Verkauf') {
+        activity = parseBuySellTransaction(pdfPage, pageIdx, 'Sell');
+        pageIdx += 6;
+      } else if (pdfPage[pageIdx] === 'Ausschüttung') {
+        activity = parseDividendTransaction(pdfPage, pageIdx, 'Dividend');
+        pageIdx += 6;
+      } else if (pdfPage[pageIdx] === 'Einbuchung') {
+        // activity = parseTxInOutTransaction(pdfPage, pageIdx, 'TransferIn');
+        pageIdx += 6;
+      } else if (pdfPage[pageIdx] === 'Ausbuchung') {
+        // activity = parseTxInOutTransaction(pdfPage, pageIdx, 'TransferOut');
+        pageIdx += 6;
+      }
+      pageIdx++;
+      if (activity !== undefined) {
+        actions.push(validateActivity(activity));
+      }
+    }
+  }
+  return actions;
+};
+
+// General parsing functions
+export const canParsePage = (content, extension) => {
+  // The first PDF Page does not always contain "Commerzbank", thus this ugly
+  // workaround. e. G. dividend_IE00B3RBWM25_1.json
+  if (!Array.isArray(content)) {
+    return undefined;
+  }
+  const joinedContent = content.join('');
+  return (
+    extension === 'pdf' &&
+    (((joinedContent.includes('COMMERZBANK') ||
+      joinedContent.includes('Commerzbank') ||
+      joinedContent.includes(
+        'SteuerlicheBehandlung:AusländischeInvestment-Ausschüttung'
+      )) &&
+    (isBuy(content) || isForeignDividend(content) || isDividend(content))) ||
+      isTransactionReport(content))
+  );
+};
+
+export const parsePages = contents => {
+
+  // Transaction Reports need to be handled completely different from individual
+  // transaction documents
+  let activities;
+  if ( isTransactionReport(contents[0])) {
+    activities = parseTransactionReport(contents);
+  }
+  else {
+    activities = [parseSingleTransaction(contents[0])];
+  }
   return {
     activities,
     status: 0,
--- tests/brokers/__mocks__/commerzbank/index.js
+++ tests/brokers/__mocks__/commerzbank/index.js
@@ -22,23 +22,9 @@ export const dividendSamples = [
   require('./dividend/dividend_foreign_currency_IE00B8GKDB10_2.json'),
 ];
 
-export const allSamples = [
-  require('./buy/buy_A1T8FV_1.json'),
-  require('./buy/buy_mixed_A1JX51_1.json'),
-  require('./buy/buy_mixed_A1JX51_2.json'),
-  require('./buy/buy_mixed_A1JX52_1.json'),
-  require('./buy/buy_mixed_A1JX52_2.json'),
-  require('./buy/buy_mixed_A1T8FV_1.json'),
-  require('./dividend/dividend_IE00B3RBWM25_1.json'),
-  require('./dividend/dividend_IE00B3RBWM25_2.json'),
-  require('./dividend/dividend_IE00B3VVMM84_1.json'),
-  require('./dividend/dividend_IE00B3VVMM84_2.json'),
-  require('./dividend/dividend_IE00B8GKDB10_1.json'),
-  require('./dividend/dividend_IE00B8GKDB10_2.json'),
-  require('./dividend/dividend_foreign_currency_IE00B3RBWM25_1.json'),
-  require('./dividend/dividend_foreign_currency_IE00B3RBWM25_2.json'),
-  require('./dividend/dividend_foreign_currency_IE00B3VVMM84_1.json'),
-  require('./dividend/dividend_foreign_currency_IE00B3VVMM84_2.json'),
-  require('./dividend/dividend_foreign_currency_IE00B8GKDB10_1.json'),
-  require('./dividend/dividend_foreign_currency_IE00B8GKDB10_2.json'),
-];
+export const transactionReport = [
+  require('./transactionReport/transaction_report_shortened_1.json'),
+
+]
+
+export const allSamples = buySamples.concat(dividendSamples, transactionReport);
