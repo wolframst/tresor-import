836869a2eb043bfe410f6ea5863777d1317fd9d5
David Dreher <david@md-web-solutions.de>
2020-11-23 23:04:10 +0100

    [baaderbank] Add another identification trait `Scalable Capital Vermögensverw.GmbH` (#251) (patch)
    * Add test case from #250
    
    * Replace allSamples with `.concat`
    
    * Documents from `Scalable Capital Vermögensverw.GmbH` will now be parsed as `scalableCapital`
    
    * Fix name handling of securities. Now both lines with name elements will parsed when available.
    
    Co-authored-by: David Dreher <dredav@users.noreply.github.com>

--- tests/brokers/__mocks__/baaderbank/index.js
+++ tests/brokers/__mocks__/baaderbank/index.js
@@ -1,21 +1,3 @@
-export const allSamples = [
-  // buys
-  require('./buy/market_order.json'),
-  require('./buy/saving_plan_vanguard_all_world.json'),
-  require('./buy/saving_plan_comstage.json'),
-  require('./buy/limit_order.json'),
-  require('./buy/biontech_from_gratisbroker.json'),
-  require('./buy/saving_plan_vanguard_from_Oskar.json'),
-
-  // sells
-  require('./sell/market_order.json'),
-
-  // dividends
-  require('./dividend/etf110_without_taxes.json'),
-  require('./dividend/dividend_USD_with_withholding_taxes.json'),
-  require('./dividend/volkswagen_with_taxes_gratisbroker.json'),
-];
-
 export const buySamples = [
   require('./buy/market_order.json'),
   require('./buy/saving_plan_vanguard_all_world.json'),
@@ -23,6 +5,7 @@ export const buySamples = [
   require('./buy/limit_order.json'),
   require('./buy/biontech_from_gratisbroker.json'),
   require('./buy/saving_plan_vanguard_from_Oskar.json'),
+  require('./buy/market_order_from_Oskar.json'),
 ];
 
 export const sellSamples = [require('./sell/market_order.json')];
@@ -32,3 +15,5 @@ export const dividendSamples = [
   require('./dividend/dividend_USD_with_withholding_taxes.json'),
   require('./dividend/volkswagen_with_taxes_gratisbroker.json'),
 ];
+
+export const allSamples = buySamples.concat(sellSamples, dividendSamples);
