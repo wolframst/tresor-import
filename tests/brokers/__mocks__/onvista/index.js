e9a30ca19a054f59fd7122c4e4e1cac202d82013
David Dreher <david@md-web-solutions.de>
2020-11-25 18:01:59 +0100

    Restructure onvista mock files (#264)
    Co-authored-by: David Dreher <dredav@users.noreply.github.com>

--- tests/brokers/__mocks__/onvista/index.js
+++ tests/brokers/__mocks__/onvista/index.js
@@ -1,22 +1,22 @@
 export const buySamples = [
-  require('./buy-1.json'),
-  require('./buy-2.json'),
-  require('./buy-3.json'),
-  require('./buy-4.json'),
+  require('./buy/2019_ComSta._MSCI_Em.Mkts.json'),
+  require('./buy/2020_Vanguard_FTSE_All_World.json'),
+  require('./buy/2018_ComSta._MSCI_World.json'),
+  require('./buy/2010_Muenchener_Rueckvers.json'),
 ];
 
 export const sellSamples = [
-  require('./sell-large.json'),
-  require('./sell-1.json'),
-  require('./sell-2.json'),
-  require('./sell-3.json'),
+  require('./sell/2020_large.json'),
+  require('./sell/2018_Deutsche_Beteiligungs_AG.json'),
+  require('./sell/2018_iShsII-J.P.M..json'),
+  require('./sell/2018_Deutsche_EuroShop_AG.json'),
 ];
 
 export const multiPageSamples = [require('./multi-page.json')];
 
 export const dividendsSamples = [
-  require('./div-1.json'),
-  require('./div-2.json'),
-  require('./div-3.json'),
-  require('./div-4.json'),
+  require('./dividend/2020_Vanguard_FTSE_All_World.json'),
+  require('./dividend/2019_iShare.NASDAQ-100.json'),
+  require('./dividend/2019_ComSta._MSCI_Em.Mkts.json'),
+  require('./dividend/2019_iSh.EO_ST.Sel.Div.30.json'),
 ];
