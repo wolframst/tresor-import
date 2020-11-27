64430aab8c017f162204a6014281bd5e98ddd034
David Dreher <david@md-web-solutions.de>
2020-11-27 15:19:12 +0100

    [degiro] Add support for sell activities and some fixes (#272) (patch)
    * Add extension check to `degiro.canParsePage`
    
    * degiro: A security name can be in more than one line, add support for sell activities, add `fxRate` and `foreignCurrency`
    
    * Add test case
    
    Co-authored-by: David Dreher <dredav@users.noreply.github.com>

--- tests/brokers/degiro.test.js
+++ tests/brokers/degiro.test.js
@@ -1,10 +1,8 @@
 import { findImplementation } from '@/index';
 import * as degiro from '../../src/brokers/degiro';
-import {
-  transactionLog
-} from './__mocks__/degiro';
+import { transactionLog } from './__mocks__/degiro';
 
-const allSamples = transactionLog//.concat(futureSamples);
+const allSamples = transactionLog; //.concat(futureSamples);
 
 describe('Broker: DEGIRO', () => {
   let consoleErrorSpy;
@@ -12,9 +10,9 @@ describe('Broker: DEGIRO', () => {
   describe('Check all documents', () => {
     test('Can the document parsed with DEGIRO', () => {
       allSamples.forEach(samples => {
-        expect(
-          samples.some(item => degiro.canParsePage(item, 'pdf'))
-        ).toEqual(true);
+        expect(samples.some(item => degiro.canParsePage(item, 'pdf'))).toEqual(
+          true
+        );
       });
     });
 
@@ -29,8 +27,9 @@ describe('Broker: DEGIRO', () => {
   });
 
   describe('Validate transactionLog', () => {
-    test('Can the transactions be parsed from buy_only_transactions', () => {
+    test('Can the transactions be parsed from: buy_only_transactions', () => {
       const activities = degiro.parsePages(transactionLog[0]).activities;
+
       expect(activities.length).toEqual(7);
       expect(activities[0]).toEqual({
         broker: 'degiro',
@@ -43,6 +42,8 @@ describe('Broker: DEGIRO', () => {
         amount: 3993.19,
         fee: 0.54,
         tax: 0,
+        foreignCurrency: 'USD',
+        fxRate: 1.1024,
       });
       expect(activities[6]).toEqual({
         broker: 'degiro',
@@ -55,6 +56,43 @@ describe('Broker: DEGIRO', () => {
         amount: 19753.76,
         fee: 25.28,
         tax: 0,
+      });
+    });
+
+    test('Can the transactions be parsed from: buy_sell_and_call_transactions', () => {
+      const activities = degiro.parsePages(transactionLog[1]).activities;
+
+      expect(activities.length).toEqual(28);
+      expect(
+        activities.filter(activity => activity !== undefined).length
+      ).toEqual(25);
+      expect(activities[5]).toEqual({
+        broker: 'degiro',
+        type: 'Buy',
+        date: '2019-05-31',
+        isin: 'SE0011527845',
+        company: 'QLINEA',
+        shares: 100,
+        price: 6.1153,
+        amount: 611.53,
+        fee: 4.36,
+        tax: 0,
+        fxRate: 10.6185,
+        foreignCurrency: 'SEK',
+      });
+      expect(activities[9]).toEqual({
+        broker: 'degiro',
+        type: 'Buy',
+        date: '2019-05-14',
+        isin: 'US9839191015',
+        company: 'XILINX INC. - COMMON',
+        shares: 8,
+        price: 100.90625,
+        amount: 807.25,
+        fee: 0.52,
+        tax: 0,
+        fxRate: 1.1226,
+        foreignCurrency: 'USD',
       });
     });
   });
