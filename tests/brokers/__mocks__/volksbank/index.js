86a9d833961025df199ecf3e6224bf830f30794f
Wolfram Stacklies <stacklies@rentablo.de>
2021-04-07 18:54:50 +0200

    volksbank

---
+++ tests/brokers/__mocks__/volksbank/index.js
@@ -0,0 +1,15 @@
+export const buySamples = [require('./buy/disney-buy-2021.json')];
+
+export const sellSamples = [require('./sell/sell-sse-2021.json')];
+
+export const dividendSamples = [
+  require('./dividend/johnson-johnson-dividend-2021.json'),
+];
+
+export const ignoredSamples = [require('./ignored/sse-ignored-page-2021.json')];
+
+export const allSamples = buySamples.concat(
+  sellSamples,
+  buySamples,
+  dividendSamples
+);
